b6cda3a00bdf50cdc87c5857367efee6df9a81ba
Alessio <alex.robassomero@gmail.com>
2021-04-09 11:56:00 +0200

    Now ships with Newtonsoft.Json.dll out of the box.

--- CFG Editor/Program.cs
+++ CFG Editor/Program.cs
@@ -1,11 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace CFG
 {
+	static class Loader {
+		public static void DynamicLoad(Assembly assembly, string dllname) {
+			string resourceName = "CFG.Resources." + dllname;
+			using (Stream s = assembly.GetManifestResourceStream(resourceName)) {
+				byte[] data = new BinaryReader(s).ReadBytes((int)s.Length);
+				string assemblyPath = Path.GetDirectoryName(assembly.Location);
+				if (assemblyPath is null) {
+					assemblyPath = Directory.GetCurrentDirectory();
+                }
+				string templDllPath = Path.Combine(assemblyPath, dllname);
+				File.WriteAllBytes(templDllPath, data);
+            }
+        }
+    }
 	static class Program
 	{
 		/// <summary>
@@ -15,6 +31,9 @@ namespace CFG
 		[STAThread]
 		static void Main(string[] args)
 		{
+			if (!File.Exists("Newtonsoft.Json.dll")) {
+				Loader.DynamicLoad(Assembly.GetExecutingAssembly(), "Newtonsoft.Json.dll");
+            }
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
 			Application.Run(new CFG_Editor(args));
